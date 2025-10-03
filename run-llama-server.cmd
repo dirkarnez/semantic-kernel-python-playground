b0232068828b2c07159dd622b2364bdb48290399
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-03 19:31:58 +0800

    - upload files

--- run-llama-server.cmd
+++ run-llama-server.cmd
@@ -4,10 +4,16 @@
 set DOWNLOADS_DIR=^
 %USERPROFILE%\Downloads
 
-@REM https://github.com/ggml-org/llama.cpp/releases/download/b6615/llama-b6615-bin-win-cpu-x64.zip
+
+
 @REM https://huggingface.co/audreyt/Taiwan-LLM-7B-v2.1-chat-GGUF/resolve/main/Taiwan-LLM-7B-v2.1-chat-Q5_K_M.gguf
 
-cd /d "%DOWNLOADS_DIR%\llama-b6615-bin-win-cpu-x64"
+@REM https://github.com/ggml-org/llama.cpp/releases/download/b6615/llama-b6615-bin-win-cpu-x64.zip
+@REM cd /d "%DOWNLOADS_DIR%\llama-b6615-bin-win-cpu-x64"
+
+@REM https://github.com/ggml-org/llama.cpp/releases/download/b6670/llama-b6670-bin-win-cuda-12.4-x64.zip
+cd /d "%DOWNLOADS_DIR%\llama-b6670-bin-win-cuda-12.4-x64"
+
 
 .\llama-server.exe --help
 .\llama-server.exe --jinja --port 8081 -m %DOWNLOADS_DIR%\Taiwan-LLM-7B-v2.1-chat-Q5_K_M.gguf
